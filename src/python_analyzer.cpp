5f60a909aa44566bce60b355199f7867fd92c9ce
Lin-Mao <linmaodev@gmail.com>
2026-01-08 10:10:00 -0700

    Update

--- src/python_analyzer.cpp
+++ src/python_analyzer.cpp
@@ -1,12 +1,14 @@
 #include "python_frame.h"
 #include <sstream>
 
+static int count = 0;
 
 std::string get_frames() {
     std::vector<PythonFrame_t> python_frames;
     get_python_frame(python_frames);
 
     std::stringstream ss;
+    ss << "PyAnalyzer Frame Index: " << count++ << std::endl;
     for (size_t i = 0; i < python_frames.size(); i++) {
         ss << "f-" << std::to_string(i) << " "
            << std::string(python_frames[i].file_name) << ":"
